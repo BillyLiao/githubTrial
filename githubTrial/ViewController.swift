4da01ab8fe5cf4569f2174ab84070e1fdc38dced
BillyLiao <hohey670@gmail.com>
2016-09-07 16:44:02 +0800

    First Commit

---
+++ githubTrial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubTrial
+//
+//  Created by 廖慶麟 on 2016/9/7.
+//  Copyright © 2016年 廖慶麟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
